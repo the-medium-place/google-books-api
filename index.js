f77d05f49b8face7a085b3f09a6d38c2d6a69856
ZGS <zgstowell@gmail.com>
2020-09-12 21:23:28 -0700

    deploy links hopefully working

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors")
-// var exphbs = require("express-handlebars");
+const cors = require("cors");
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3001
 
+const PORT = process.env.PORT || 3001;
 
 const db = require("./models");
 
@@ -13,21 +13,31 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks_db", {
+mongoose.connect(process.env.REACT_APP_MONGODB_URI || "mongodb://localhost/googlebooks_db",
+ {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
+// console.log(process.env.REACT_APP_MONGODB_URI)
+
 // LOCAL DEV LINK
-app.use(cors({
-    origin:["http://localhost:3000"]
-}));
+// app.use(cors({
+//     origin:["http://localhost:3000"]
+// }));
 
 
 // DEPLOYED LINK
 // app.use(cors({
 //     origin:["https://pacific-earth-75465.herokuapp.com"]
 // }));
+
+// DEPLOYED LINK
+app.use(cors({
+    origin:["https://awesome-google-books-react.herokuapp.com/"]
+}));
+
+
 
 app.get("/", (req, res) => {
     res.send("API splash!")
@@ -37,9 +47,9 @@ app.get("/api/favbooks", (req,res) => {
     console.log('received request')
 
     db.FavBook.find({})
-    .then(dbWorkout => {   
+    .then(dbFavBook => {   
 
-        res.json(dbWorkout);
+        res.json(dbFavBook);
     })
     .catch(err => {
         res.json(err);
@@ -49,8 +59,8 @@ app.get("/api/favbooks", (req,res) => {
 
 
 app.post("/api/favbooks", ({ body }, res) => {
-    console.log(body.volumeInfo.title);
-    console.log(body.volumeInfo);
+    // console.log(body.volumeInfo.title);
+    // console.log(body.volumeInfo);
     const newObj = {
         title: body.volumeInfo.title,
         subTitle: body.volumeInfo.subtitle,
@@ -66,7 +76,7 @@ app.post("/api/favbooks", ({ body }, res) => {
             res.send(dbfavbook);
         })
         .catch(err => {
-            console.log(err);
+            console.error(err);
             res.send(err);
         })
 })
